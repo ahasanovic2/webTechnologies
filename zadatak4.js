1622ef1921c78a8cf8e5bc8a86c1576c44e63d06
Ahmedin Hasanović <ahasanovic2@etf.unsa.ba>
2022-02-19 11:37:08 +0100

    Spirala 2
    Spirala 2

---
+++ zadatak4.js
@@ -0,0 +1,831 @@
+var tekst1=`{
+    "stats": {
+      "suites": 2,
+      "tests": 2,
+      "passes": 2,
+      "pending": 0,
+      "failures": 0,
+      "start": "2021-11-05T15:00:26.343Z",
+      "end": "2021-11-05T15:00:26.352Z",
+      "duration": 9
+    },
+    "tests": [
+      {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      },
+      {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      }
+    ],
+    "pending": [],
+    "failures": [],
+    "passes": [
+    {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        }
+    ]
+  }
+  `;
+  var tekst2=`{
+    "stats": {
+      "suites": 2,
+      "tests": 2,
+      "passes": 2,
+      "pending": 0,
+      "failures": 0,
+      "start": "2021-11-05T15:00:26.343Z",
+      "end": "2021-11-05T15:00:26.352Z",
+      "duration": 9
+    },
+    "tests": [
+      {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      },
+      {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      }
+    ],
+    "pending": [],
+    "failures": [],
+    "passes": [
+    {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        }
+    ]
+}`;
+
+var tekst3=`{
+    "stats": {
+      "suites": 2,
+      "tests": 4,
+      "passes": 4,
+      "pending": 0,
+      "failures": 0,
+      "start": "2021-11-05T15:00:26.343Z",
+      "end": "2021-11-05T15:00:26.352Z",
+      "duration": 9
+    },
+    "tests": [
+      {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      },
+      {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      },
+      {
+        "title": "Test 3",
+        "fullTitle": "Test 3",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      },
+      {
+        "title": "Test 4",
+        "fullTitle": "Test 4",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      }
+    ],
+    "pending": [],
+    "failures": [],
+    "passes": [
+        {
+            "title": "Test 1",
+            "fullTitle": "Test 1",
+            "file": null,
+            "duration": 1,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 2",
+            "fullTitle": "Test 2",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 3",
+            "fullTitle": "Test 3",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 4",
+            "fullTitle": "Test 4",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          }
+    ]
+  }
+  `;
+  var tekst4=`{
+    "stats": {
+      "suites": 2,
+      "tests": 4,
+      "passes": 2,
+      "pending": 0,
+      "failures": 2,
+      "start": "2021-11-05T15:00:26.343Z",
+      "end": "2021-11-05T15:00:26.352Z",
+      "duration": 9
+    },
+    "tests": [
+        {
+            "title": "Test 1",
+            "fullTitle": "Test 1",
+            "file": null,
+            "duration": 1,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 2",
+            "fullTitle": "Test 2",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 3",
+            "fullTitle": "Test 3",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 4",
+            "fullTitle": "Test 4",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          }
+    ],
+    "pending": [],
+    "failures": [
+        {
+            "title": "Test 3",
+            "fullTitle": "Test 3",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 4",
+            "fullTitle": "Test 4",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          }
+    ],
+    "passes": [
+    {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        }
+    ]
+  }`;
+
+var tekst5=`{
+"stats": {
+    "suites": 2,
+    "tests": 3,
+    "passes": 2,
+    "pending": 0,
+    "failures": 1,
+    "start": "2021-11-05T15:00:26.343Z",
+    "end": "2021-11-05T15:00:26.352Z",
+    "duration": 9
+},
+"tests": [
+    {
+    "title": "Test 1",
+    "fullTitle": "Test 1",
+    "file": null,
+    "duration": 1,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    },
+    {
+    "title": "Test 2",
+    "fullTitle": "Test 2",
+    "file": null,
+    "duration": 0,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    },
+    {
+    "title": "Test 3",
+    "fullTitle": "Test 3",
+    "file": null,
+    "duration": 0,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    }
+],
+"pending": [],
+"failures": [
+    {
+    "title": "Test 3",
+    "fullTitle": "Test 3",
+    "file": null,
+    "duration": 0,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    }
+],
+"passes": [
+    {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        }
+]
+}
+`;
+var tekst6=`{
+"stats": {
+    "suites": 2,
+    "tests": 4,
+    "passes": 2,
+    "pending": 0,
+    "failures": 2,
+    "start": "2021-11-05T15:00:26.343Z",
+    "end": "2021-11-05T15:00:26.352Z",
+    "duration": 9
+},
+"tests": [
+    {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 3",
+        "fullTitle": "Test 3",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 4",
+        "fullTitle": "Test 4",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        }
+],
+"pending": [],
+"failures": [
+    {
+        "title": "Test 3",
+        "fullTitle": "Test 3",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 4",
+        "fullTitle": "Test 4",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        }
+],
+"passes": [
+{
+    "title": "Test 1",
+    "fullTitle": "Test 1",
+    "file": null,
+    "duration": 1,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    },
+    {
+    "title": "Test 2",
+    "fullTitle": "Test 2",
+    "file": null,
+    "duration": 0,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    }
+]
+}`;
+
+var tekst7=`{
+"stats": {
+    "suites": 2,
+    "tests": 3,
+    "passes": 2,
+    "pending": 0,
+    "failures": 1,
+    "start": "2021-11-05T15:00:26.343Z",
+    "end": "2021-11-05T15:00:26.352Z",
+    "duration": 9
+},
+"tests": [
+    {
+    "title": "Test 1",
+    "fullTitle": "Test 1",
+    "file": null,
+    "duration": 1,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    },
+    {
+    "title": "Test 2",
+    "fullTitle": "Test 2",
+    "file": null,
+    "duration": 0,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    },
+    {
+    "title": "Test 5",
+    "fullTitle": "Test 5",
+    "file": null,
+    "duration": 0,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    }
+],
+"pending": [],
+"failures": [
+    {
+    "title": "Test 5",
+    "fullTitle": "Test 5",
+    "file": null,
+    "duration": 0,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    }
+],
+"passes": [
+    {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        }
+]
+}
+`;
+var tekst8=`{
+"stats": {
+    "suites": 2,
+    "tests": 4,
+    "passes": 2,
+    "pending": 0,
+    "failures": 2,
+    "start": "2021-11-05T15:00:26.343Z",
+    "end": "2021-11-05T15:00:26.352Z",
+    "duration": 9
+},
+"tests": [
+    {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 3",
+        "fullTitle": "Test 3",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 4",
+        "fullTitle": "Test 4",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        }
+],
+"pending": [],
+"failures": [
+    {
+        "title": "Test 3",
+        "fullTitle": "Test 3",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 4",
+        "fullTitle": "Test 4",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        }
+],
+"passes": [
+{
+    "title": "Test 1",
+    "fullTitle": "Test 1",
+    "file": null,
+    "duration": 1,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    },
+    {
+    "title": "Test 2",
+    "fullTitle": "Test 2",
+    "file": null,
+    "duration": 0,
+    "currentRetry": 0,
+    "speed": "fast",
+    "err": {}
+    }
+]
+}`;
+var tekst9=`{
+    "stats": {
+      "suites": 2,
+      "tests": 4,
+      "passes": 2,
+      "pending": 0,
+      "failures": 2,
+      "start": "2021-11-05T15:00:26.343Z",
+      "end": "2021-11-05T15:00:26.352Z",
+      "duration": 9
+    },
+    "tests": [
+      {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      },
+      {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      },
+      {
+        "title": "Test 6",
+        "fullTitle": "Test 6",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      },
+      {
+        "title": "Test 5",
+        "fullTitle": "Test 5",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+      }
+    ],
+    "pending": [],
+    "failures": [
+        {
+            "title": "Test 6",
+            "fullTitle": "Test 6",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 5",
+            "fullTitle": "Test 5",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          }
+    ],
+    "passes": [
+        {
+            "title": "Test 1",
+            "fullTitle": "Test 1",
+            "file": null,
+            "duration": 1,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 2",
+            "fullTitle": "Test 2",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          }
+    ]
+  }
+  `;
+  var tekst10=`{
+    "stats": {
+      "suites": 2,
+      "tests": 4,
+      "passes": 2,
+      "pending": 0,
+      "failures": 2,
+      "start": "2021-11-05T15:00:26.343Z",
+      "end": "2021-11-05T15:00:26.352Z",
+      "duration": 9
+    },
+    "tests": [
+        {
+            "title": "Test 1",
+            "fullTitle": "Test 1",
+            "file": null,
+            "duration": 1,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 2",
+            "fullTitle": "Test 2",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 4",
+            "fullTitle": "Test 4",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 3",
+            "fullTitle": "Test 3",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          }
+    ],
+    "pending": [],
+    "failures": [
+        {
+            "title": "Test 4",
+            "fullTitle": "Test 4",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          },
+          {
+            "title": "Test 3",
+            "fullTitle": "Test 3",
+            "file": null,
+            "duration": 0,
+            "currentRetry": 0,
+            "speed": "fast",
+            "err": {}
+          }
+    ],
+    "passes": [
+    {
+        "title": "Test 1",
+        "fullTitle": "Test 1",
+        "file": null,
+        "duration": 1,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        },
+        {
+        "title": "Test 2",
+        "fullTitle": "Test 2",
+        "file": null,
+        "duration": 0,
+        "currentRetry": 0,
+        "speed": "fast",
+        "err": {}
+        }
+    ]
+  }`;
+let assert = chai.assert;
+describe('TestoviParser', function() {
+    describe('Testovi metode porediRezultate', function() {
+        it('Isti testovi u oba reporta', function() {
+              assert.equal(JSON.stringify(TestoviParser.porediRezultate(tekst1,tekst2)),'{"promjena":"100%","greske":[]}');
+        });
+
+        it('Isti testovi u oba reporta, ali sa različitom prolaznošću', function() {
+              assert.equal(JSON.stringify(TestoviParser.porediRezultate(tekst3,tekst4)),'{"promjena":"50%","greske":["Test 3","Test 4"]}');
+        });
+        
+        it('Prvi report nema test 4', function() {
+              assert.equal(JSON.stringify(TestoviParser.porediRezultate(tekst5,tekst6)),'{"promjena":"50%","greske":["Test 3","Test 4"]}');
+        });
+
+        it('Prvi report ima test 5 koji pada, a kojeg nema drugi report', function() {
+              assert.equal(JSON.stringify(TestoviParser.porediRezultate(tekst7,tekst8)),'{"promjena":"60%","greske":["Test 5","Test 3","Test 4"]}');
+        });
+
+        it('Provjeravamo sortiranje testova', function() {
+              assert.equal(JSON.stringify(TestoviParser.porediRezultate(tekst9,tekst10)),'{"promjena":"66.7%","greske":["Test 5","Test 6","Test 3","Test 4"]}');
+        });
+    });
+});
